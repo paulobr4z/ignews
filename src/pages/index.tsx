a37a7c037296d24ea450c15ebb62c8cac1b1f371
paulobr4z <paulobrazaraujo@gmail.com>
2021-11-22 22:52:27 -0300

    add component Header

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello World!
+      </h1>
+    </>
+  )
+}
